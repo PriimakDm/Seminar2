0c48eff8e8d9d9e4c1018dcd6f247f5eb16e8403
Dmitrii Priimak <priimak69@inbox.ru>
2023-06-17 14:54:41 +0700

    Task_1

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// На вход трёхзначное число и на выходе показывает вторую цифру этого числа
+Console.WriteLine("input a three-digit number: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+if (namber < 99 || namber > 999)
+{
+    Console.WriteLine("Error");
+}
+else
+{
+    int digit = ((namber % 100) / 10);
+    Console.WriteLine("second digit: ");
+    Console.Write(digit);
+}
