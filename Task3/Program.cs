282fe007f1ee3de151ad1fed473d64406d6650eb
Dmitrii Priimak <priimak69@inbox.ru>
2023-06-17 17:37:40 +0700

    Task_3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿// Принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+Console.WriteLine("Press Inter");
+ConsoleKey key = Console.ReadKey().Key;
+if (key == ConsoleKey.Enter) {
+int day = new Random().Next(1, 7);
+if (day == 6 || day == 7)
+{
+    Console.WriteLine(day);
+    Console.WriteLine("Day off");
+}
+else {
+    Console.WriteLine(day);
+    Console.WriteLine("Weekday");
+}
+}
+   
