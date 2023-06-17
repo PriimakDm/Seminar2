930ca6a259b3a910ac28e79a8e1bdffa3a50851b
Dmitrii Priimak <priimak69@inbox.ru>
2023-06-17 16:36:56 +0700

    Task_2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+Console.WriteLine("input number: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+int div = 10;
+int prob = (namber / 10);
+int rem = (namber % div);
+if (prob < 10)
+{
+    Console.WriteLine("The third digit is missing");
+}
+if (namber > 99 && namber < 1000)
+{
+Console.WriteLine("The third digit is  ");
+Console.WriteLine(rem);
+}
